754f926a1f15fabccc0e443afbb8f1f7affdecad
giotodsco <todescogiovanni18@gmail.com>
2026-03-04 10:17:24 -0300

    feat: initial project setup with Spring Boot

---
+++ src/main/java/financial/dev/simplified_payment_api/SimplifiedPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package financial.dev.simplified_payment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/financial/dev/simplified_payment_api/SimplifiedPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package financial.dev.simplified_payment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
