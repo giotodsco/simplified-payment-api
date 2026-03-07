9afa2a54da4065fcc9c332bbc7b1a14c57f54f53
giotodsco <todescogiovanni18@gmail.com>
2026-03-07 14:13:45 -0300

    feat: add UserController with create and list endpoints

---
+++ src/main/java/financial/dev/simplified_payment_api/controller/UserController.java
@@ -0,0 +1,29 @@
+package financial.dev.simplified_payment_api.controller;
+
+import financial.dev.simplified_payment_api.dtos.request.UserRequest;
+import financial.dev.simplified_payment_api.dtos.response.UserResponse;
+import financial.dev.simplified_payment_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> listarAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.listarAll());
+    }
+}
